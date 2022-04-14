bba72baa8b45088d39e3d5eea37a29de8a3c9101
Wayne-Kelsick <wkelsick@protonmail.com>
2022-04-14 07:34:27 -0700

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -9,6 +9,22 @@ const Header = ({ siteTitle }) => (
       marginBottom: `1.45rem`,
     }}
   >
+    <script src="https://cdn.tailwindcss.com"></script>
+	  <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.1/dist/flowbite.min.css" />
+	  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	  <script src="https://unpkg.com/@metamask/detect-provider/dist/detect-provider.min.js"></script>
+	  <script type="text/javascript">
+		  const provider = detectEthereumProvider()
+	  
+		  if (provider) {
+		    // handle provider
+		  } else {
+		    // handle no provider
+		  }
+	  </script>
+
+
+	<title>ORPHEUS Web App</title>
     <div
       style={{
         margin: `0 auto`,
